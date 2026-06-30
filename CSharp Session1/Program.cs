1af41ff6f85f99f7dea52b4d172ddb2b0e40ae9c
saffa <user@DESKTOP-6E3PP0S>
2026-06-30 22:42:47 +0400

    Add project files.

---
+++ CSharp Session1/Program.cs
@@ -0,0 +1,120 @@
+﻿namespace CSharp_Session1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //program to show welcome message 
+            /*
+             hello 
+            spark to code session explanation
+
+             
+            Console.WriteLine("Please Enter your Name: ");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("Please Enter your Age: ");
+            int userAge = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Please enter your salary: ");
+            float userSalary=float.Parse(Console.ReadLine());
+
+  
+            Console.WriteLine("Hello "+userName+ " welcome to spark!"); */
+
+            //Console.WriteLine("Enter first number: ");
+            //float firstNumber=float.Parse(Console.ReadLine());
+
+            //Console.WriteLine("Enter Second  number: ");
+            //float secondNumber = float.Parse(Console.ReadLine());
+
+            //float additionresult = firstNumber + secondNumber;
+            //float subtractionresult = firstNumber - secondNumber;
+            //float multiplicationresult = firstNumber * secondNumber;
+            //float divisionresult = firstNumber / secondNumber;
+            //float reminderresult= firstNumber % secondNumber;
+            //bool comparisonresult= firstNumber == secondNumber;
+
+            //bool testresult != (10 > 5);//false
+
+            //Console.WriteLine("Addtion result: "+ additionresult);
+            //Console.WriteLine("substration result: " + subtractionresult);
+            //Console.WriteLine("multiplication result: " + multiplicationresult);
+            //Console.WriteLine("division result: " + divisionresult);
+            //Console.WriteLine("reminder result: " + reminderresult);
+            //Console.WriteLine("comparison  result: " + comparisonresult);
+
+            //bool result = firstNumber > secondNumber;
+
+            //and: &&
+            //Or:||
+            //not:|
+
+            //Console.WriteLine("Enter your degree: ");
+            //float degree=float.Parse(Console.ReadLine());
+            //if (degree < 50)
+            //{
+            //    Console.WriteLine("you failed");
+
+            //}
+            //else if (degree >=50 && degree<60)
+            //{ 
+            //    Console.WriteLine("you passed with grade D");
+            //}
+            //else if (degree >= 60 && degree < 70)
+            //{
+            //    Console.WriteLine("you passed with grade C");
+
+            //}
+            //else if (degree >= 70 && degree < 80)
+            //{
+            //    Console.WriteLine("you passed with grade B");
+
+            //}
+            //else if (degree >= 80 && degree < 90)
+            //{
+            //    Console.WriteLine("you passed with grade A");
+
+            //}
+            //else
+            //{
+            //    Console.WriteLine("you entered wrong number");
+
+            //}
+
+            //Console.WriteLine("Welcome to main menu: ");
+            //Console.WriteLine("1.deposite");
+            //Console.WriteLine("2.withdraw");
+            //Console.WriteLine("3.balance check");
+            //Console.WriteLine("please choose option");
+            //int option=int.Parse(Console.ReadLine());
+            //switch(option)
+            //{
+            //    case 1:
+            //        Console.WriteLine("deposited");
+            //        break:
+            //    case 2:
+            //        Console.WriteLine("withdrawed");
+            //        break:
+            //    case 3:
+            //        Console.WriteLine("balance check:");
+            //        break:
+                
+            //    default:
+            //        Console.WriteLine("please choose correct option");
+
+            
+            //}
+
+
+
+
+
+
+
+
+
+
+        }
+    }
+}

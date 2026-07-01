a76300cddacd3fc0d1626f710cdd18f9fc4febae
saffa <safahamood03@gmail.com>
2026-07-01 10:55:48 +0400

    adding comment testline

---
+++ Task1/Task1/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ////Task 1:
+            //string Name = "Noor";
+            //double Height = 1.25;
+            //int Age = 20;
+            //bool isStudent = true;
+
+            //Console.WriteLine("my name is  " + Name);
+            //Console.WriteLine("im  " + Age+ " years old");
+            //Console.WriteLine("im  " + Height+ " cm");
+            //Console.WriteLine(" Im student : " + isStudent);
+
+            //Task2:
+
+            
+
+        }
+    }
+}

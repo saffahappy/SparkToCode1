5fc9daa1451f622f0c1bb324348346817ab6e090
saffa <safahamood03@gmail.com>
2026-07-09 16:03:11 +0400

    enter all solved Q1 fixed grade array

---
+++ task5/Task5-solution/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Task5_solution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}

ccfce74b486cb74d6c60ebb972e8360298f18836
saffa <safahamood03@gmail.com>
2026-07-13 09:39:16 +0400

    solve Q1

--- Task 5/Program.cs
+++ Task 5/Program.cs
@@ -1,10 +1,23 @@
-﻿namespace Task_5
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿////Q1 fixed grade array
+//using System;
+
+//internal class Program
+//{
+//    static void Main(string[] args)
+//    {
+//        int[] grades = new int[5];
+
+//        for (int i = 0; i < grades.Length; i++)
+//        {
+//            Console.Write("Enter grade " + (i + 1));
+//            grades[i] = Convert.ToInt32(Console.ReadLine());
+//        }
+
+//        Console.WriteLine("Student Grades:");
+
+//        foreach (int grade in grades)
+//        {
+//            Console.WriteLine(grade);
+//        }
+//    }
+//}

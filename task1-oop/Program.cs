ac3ba81024c93c351072039d1638554231f684ae
saffa <safahamood03@gmail.com>
2026-07-14 11:38:00 +0400

    progrmmed bank account class

--- task1-oop/Program.cs
+++ task1-oop/Program.cs
@@ -1,10 +1,64 @@
-﻿namespace task1_oop
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
-    }
-}
+﻿//namespace task1_oop
+//{
+//    internal class BankAccount
+//    {
+//        public int AccountNumber { get; set; }
+//        public string HolderName { get; set; }
+//        public double Balance { get; set; }
+
+//        // Case 16
+//        public BankAccount()
+//        {
+
+//        }
+
+//        public BankAccount(int accountNumber, string holderName, double balance)
+//        {
+//            AccountNumber = accountNumber;
+//            HolderName = holderName;
+//            Balance = balance;
+//        }
+
+//        // Case 18
+//        public bool IsOverdrawn
+//        {
+//            get { return Balance < 0; }
+//        }
+
+//        public void Deposit(double amount)
+//        {
+//            Balance += amount;
+//            SendEmail();
+//        }
+
+//        public void Withdraw(double amount)
+//        {
+//            if (Balance >= amount)
+//            {
+//                Balance -= amount;
+//                SendEmail();
+//            }
+//            else
+//            {
+//                Console.WriteLine("not enough balance.");
+//            }
+//        }
+
+//        public double CheckBalance()
+//        {
+//            PrintInformation();
+//            return Balance;
+//        }
+
+//        private void PrintInformation()
+//        {
+//            Console.WriteLine($"holder Name : {HolderName}");
+//            Console.WriteLine($"balance     : {Balance}");
+//        }
+
+//        private void SendEmail()
+//        {
+//            Console.WriteLine("Email notification is sent.");
+//        }
+//    }
+//}

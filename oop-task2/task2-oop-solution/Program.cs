bc007d0c9e38d0c9ce046a1a93adb7f215c5419e
saffa <safahamood03@gmail.com>
2026-07-15 09:01:49 +0400

    add code line project

---
+++ oop-task2/task2-oop-solution/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace task2_oop_solution
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello!");
+        }
+    }
+}

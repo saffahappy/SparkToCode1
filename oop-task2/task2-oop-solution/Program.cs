f74fe99ff7f2f527171b4bcb8bf0c3f47be1dfd3
saffa <safahamood03@gmail.com>
2026-07-15 11:16:07 +0400

    class room coded

--- oop-task2/task2-oop-solution/Program.cs
+++ oop-task2/task2-oop-solution/Program.cs
@@ -1,10 +1,2 @@
 ﻿namespace task2_oop_solution
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello!");
-        }
-    }
-}
+
